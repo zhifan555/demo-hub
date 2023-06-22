7120c5778dcaa43054da886099d71aa6deee78a3
zj <1576446679@qq.com>
2023-06-22 20:29:31 +0800

    Initial commit

---
+++ src/main/java/com/example/demohub/DemoHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohub/DemoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
