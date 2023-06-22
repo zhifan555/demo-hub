bbcb865b72b0d8ff240c3c7735fff26826e22039
zj <1576446679@qq.com>
2023-06-22 23:04:13 +0800

    2304

--- src/main/java/com/example/demohub/DemoHubApplication.java
+++ src/main/java/com/example/demohub/DemoHubApplication.java
@@ -13,6 +13,7 @@ public class DemoHubApplication {
         return "hello";
 
 
+
     }
     public static void main(String[] args) {
         SpringApplication.run(DemoHubApplication.class, args);
